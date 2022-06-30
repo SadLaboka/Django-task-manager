c8076f9b8d60f19be7018a15b224bef424bd974b
SadLaboka <jitubuh@gmail.com>
2022-06-30 14:54:10 +0300

    fix settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = int(os.getenv("DEBUG", default=1))
 
 ALLOWED_HOSTS = ['task-manager-sadlaboka.herokuapp.com', '127.0.0.1']
 
